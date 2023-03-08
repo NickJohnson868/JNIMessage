396a1c5404e510320c12905dc47615b1a7f1b3fa
劲松 孙 <624536826@qq.com>
2023-03-08 10:39:42 +0800

    增加

--- java-msg/src/main/java/me/johnson/msg/Main.java
+++ java-msg/src/main/java/me/johnson/msg/Main.java
@@ -6,7 +6,6 @@ public class Main {
     public static void main(String[] args) {
         WMListener wmListener = new WMListener();
         wmListener.start();
-
         wmListener.sendCopyDataMsg("{\"msg\":\"来自java-msg的消息\",\"className\":\""
                 + wmListener.getClassName() + "\"}");
     }
